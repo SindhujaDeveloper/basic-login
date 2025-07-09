981fa9f07bfad25f0fc8386f841fed9eedf727c1
Sindhuja <115704608+SindhujaDeveloper@users.noreply.github.com>
2025-07-09 23:49:34 +0530

    Update index.tsx

--- src/container/carouselComponent/index.tsx
+++ src/container/carouselComponent/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Carousel } from 'react-bootstrap'
 
-import banner1 from '../../assets/images/banner.png'
-import banner from '../../assets/images/banner_1.png'
+// import banner1 from '../../assets/images/banner.png'
+// import banner from '../../assets/images/banner_1.png'
 
 import 'assets/stylesheets/carouselComponent.scss'
 
@@ -10,18 +10,18 @@ const CarouselComponent: React.FC = () => {
   return (
     <Carousel data-bs-theme="dark">
       <Carousel.Item interval={1000} className='banner'>
-        <img
+{/*         <img
           className="d-block carousel-container"
           src={banner}
           alt="First slide"
-        />
+        /> */}
       </Carousel.Item>
       <Carousel.Item interval={1000} className='banner-1'>
-        <img
+{/*         <img
           className="d-block carousel-container"
           src={banner1}
           alt="Second slide"
-        />
+        /> */}
       </Carousel.Item>
     </Carousel>
   )
